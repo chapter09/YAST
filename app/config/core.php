7be8420a60584334c8ce86cf92d4182927b3b74d
Lou Jian <gsj987@gmail.com>
2012-08-14 23:33:15 +0800

    nothing to commit

--- app/config/core.php
+++ app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '$apr1$SDgLvRh7$v1n21KC/jiy4dluaMppRy');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '44012705331690917100018251367656');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
